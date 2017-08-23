77508d5544a1ecafe9126af70c730012c72361e3
Kharismaray <31243138+Kharismaray@users.noreply.github.com>
2017-08-23 22:05:11 +0700

    Add files via upload

---
+++ 1_KHR.py
@@ -0,0 +1,8 @@
+nama = input("Masukan nama kamu:")
+print("Nama saya adalah: " + nama)
+
+umur = input("Masukan umur kamu:")
+print("Umur saya adalah: " + umur)
+
+Jenis_kelamin = input("Masukan jenis kelamin kamu:")
+print ("Jenis kelamin saya adalah: " + Jenis_kelamin)

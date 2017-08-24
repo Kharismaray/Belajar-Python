891cc5e3ac445341e4110ef7ad37e0d6169f7f06
Esa Firman <esafirm21@gmail.com>
2017-08-24 20:34:35 +0700

    Add Esa version

---
+++ 1_Esa.py
@@ -0,0 +1,9 @@
+name = input("Siapa nama kamu?")
+umur = input("Berapa umur kamu?")
+jk = input("Apa jenis kelaminmu? (L/P)")
+
+print("""
+    Nama saya: {}
+    Umur saya: {}
+    Jenis kelamin saya: {}
+    """.format(name, umur, jk))
